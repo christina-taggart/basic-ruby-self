e8845be692f723c99c8a6bf52f76eabc97aaff9b
Kevin Yeh & Eli Shkurkin <pair+kyeh.shkurkin@devbootcamp.com>
2014-01-13 12:18:59 -0800

    Release 0: Add driver code for person class and math method

--- source/self.rb
+++ source/self.rb
@@ -19,17 +19,31 @@ end
 
 ## Write your driver code to run both methods above here:
 
+new_person = Person.new
+new_person.example_instance_method
+
+Person.example_class_method
+
 ## MODULE CONTEXT
 ## The following are identical.  Comment each out separately and write the driver code to run the hypot method
 
 module Math
   def self.hypot(a, b)
   # maths in here
+  p "I'm doings maths"
   end
 end
 
-# module Math
-#   def Math.hypot(a, b)
-#     # maths in here
-#   end
-# end
+module Math2
+  def Math2.hypot(a, b)
+    # maths in here
+    p "I'm doings maths 2"
+  end
+end
+
+puts "Calling by self.hypot"
+Math::hypot(1,2)
+
+puts "Calling by Math2.hypot"
+Math2::hypot(3,4)
+
