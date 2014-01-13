b41cbffa00232b238bcd5825479daec56264fe4b
Alexander Leishman & Ricardo A. Rubio <pair+leishman.rickarubio@devbootcamp.com>
2014-01-13 15:35:07 -0800

    release 1: ruby drill showcasing self keyword usage completed

--- source/self.rb
+++ source/self.rb
@@ -19,17 +19,26 @@ end
 
 ## Write your driver code to run both methods above here:
 
+Person.example_class_method
+
+rick = Person.new
+rick.example_instance_method
+
 ## MODULE CONTEXT
 ## The following are identical.  Comment each out separately and write the driver code to run the hypot method
 
+# module Math
+#   def self.hypot(a, b)
+#     self.sqrt(a ** 2 + b ** 2)
+#   end
+# end
+
 module Math
-  def self.hypot(a, b)
-  # maths in here
+  def Math.hypot(a, b)
+    self.sqrt(a ** 2 + b ** 2)
   end
 end
 
-# module Math
-#   def Math.hypot(a, b)
-#     # maths in here
-#   end
-# end
+p Math.hypot(3,4) == 5
+
+# Self refers to the receiver object of the current method.
