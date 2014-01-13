f105813b92fd84ef0e76b5f777a3c78db1be265c
Apprentice <apprentice@dbc05.local>
2014-01-13 12:37:07 -0800

    Release 1

--- source/self.rb
+++ source/self.rb
@@ -19,17 +19,29 @@ end
 
 ## Write your driver code to run both methods above here:
 
+me = Person.new
+Person.example_class_method
+me.example_instance_method
+
 ## MODULE CONTEXT
 ## The following are identical.  Comment each out separately and write the driver code to run the hypot method
 
 module Math
   def self.hypot(a, b)
-  # maths in here
+    return (a*a) + (b*b)
   end
 end
 
 # module Math
 #   def Math.hypot(a, b)
-#     # maths in here
+#     return (a*a) + (b*b)
 #   end
 # end
+
+p Math.hypot(3,4)
+
+=begin
+ The "self" keyword is used to refrence the current item that we are interacting with.
+ This could be a class, a method, or an object.
+
+=end
