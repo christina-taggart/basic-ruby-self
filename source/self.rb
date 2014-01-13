4c95a9b7456b02c20269014288a3858b1b06b5e4
Roy Lee & Nicholas Cu <pair+lee-sh-roy.nicholascu@devbootcamp.com>
2014-01-13 15:58:10 -0800

    Added explainations.

--- source/self.rb
+++ source/self.rb
@@ -18,18 +18,33 @@ class Person
 end
 
 ## Write your driver code to run both methods above here:
+p Person.example_class_method
+p Person.new.example_instance_method
+nickc = Person.new
+nickc.example_instance_method
 
 ## MODULE CONTEXT
 ## The following are identical.  Comment each out separately and write the driver code to run the hypot method
 
 module Math
   def self.hypot(a, b)
-  # maths in here
+    sqrt(a**2 + b**2)
   end
 end
 
-# module Math
-#   def Math.hypot(a, b)
-#     # maths in here
-#   end
-# end
+p Math.hypot(3, 4)
+
+module Math
+  def Math.hypot(a, b)
+    sqrt(a**2 + b**2)
+  end
+end
+
+p Math.hypot(3, 4)
+
+# Class methods are used as a general idea among things
+# Instance methods are called on specific examples of code.
+# Self is used, then, to refer either to the general Class (in the example of self.example_class_method)
+# or to the instance when not specified (e.g., example_instance_method)
+
+# In the reference to Modules, there is no difference.
